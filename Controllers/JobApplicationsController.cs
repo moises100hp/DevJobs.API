75a87c6c9321b400d28d9025126289213600b5b9
Moisés Gonçalves Silva <moises50hp@hotmail.com>
2022-05-03 23:30:11 -0300

    Add project files.

---
+++ Controllers/JobApplicationsController.cs
@@ -0,0 +1,38 @@
+﻿using DevJobs.API.Entities;
+using DevJobs.API.Models;
+using DevJobs.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevJobs.API.Controllers
+{
+    [Route("api/job-vacancies/{id}/application")]
+    [ApiController]
+    public class JobApplicationsController : ControllerBase
+    {
+        private readonly DevJobsContext _context;
+        public JobApplicationsController(DevJobsContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/job-vacancies/4/applications
+        [HttpPost]
+        public IActionResult Post(int id, AddJobApplicationInputModel model)
+        {
+            var jobVacancy = _context.JobVacancies
+                .SingleOrDefault(jv => jv.Id == id);
+
+            if (jobVacancy == null)
+                return NotFound();
+
+            var application = new JobApplication(
+                model.ApplicantName,
+                model.ApplicantEmail,
+                id);
+
+            jobVacancy.Applications.Add(application);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Controllers/JobVacanciesController.cs
@@ -0,0 +1,75 @@
+﻿using DevJobs.API.Entities;
+using DevJobs.API.Models;
+using DevJobs.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevJobs.API.Controllers
+{
+    [Route("api/job-vacancies)")]
+    [ApiController]
+    public class JobVacanciesController : ControllerBase
+    {
+        private readonly DevJobsContext _context;
+        public JobVacanciesController(DevJobsContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/job-vacancies
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var jobVacancies = _context.JobVacancies;
+
+            return Ok(jobVacancies);
+        }
+
+        // GET api/job-vacancies/4
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var jobVacancy = _context.JobVacancies
+                .SingleOrDefault(jv => jv.Id == id);
+
+            if (jobVacancy == null)
+                return NotFound();
+
+            return Ok(jobVacancy);
+        }
+
+        // POST api/job-vacancies
+        [HttpPost]
+        public IActionResult Post(AddJobVacancyInputModel model)
+        {
+            var jobVacancy = new JobVacancy(
+                model.Title,
+                model.Description,
+                model.Company,
+                model.IsRemote,
+                model.SalaryRange
+                );
+
+            _context.JobVacancies.Add(jobVacancy);
+
+            return CreatedAtAction(
+                "GetById",
+                new { id = jobVacancy.Id },
+                jobVacancy);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateJobVacancyInputModel model)
+        {
+            var jobVacancy = _context.JobVacancies
+              .SingleOrDefault(jv => jv.Id == id);
+
+            if (jobVacancy == null)
+                return NotFound();
+
+            jobVacancy.Update(model.Title, model.Description);
+
+            return NoContent();
+        }
+
+    }
+}
